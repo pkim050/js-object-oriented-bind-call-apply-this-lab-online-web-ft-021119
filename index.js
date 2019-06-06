44d8e24cecf29b76b1be441df46f1b5fad7a8d1c
Patrick Kim <pkim050@gmail.com>
2019-06-06 22:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, fn2, integer) {
   return fn.call(fn2, integer);
 }
+
+function setThisWithApply(fn, fn2, array) {
+  return fn.call(fn2, array);
+}
