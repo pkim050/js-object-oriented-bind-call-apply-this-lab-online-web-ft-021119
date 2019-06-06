b02bb489586d009073d049a46b457cb914fd612c
Patrick Kim <pkim050@gmail.com>
2019-06-06 22:17:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, fn2, integer) {
+  return fn.call(fn2, integer);
+}
+
+function setThisWithApply(fn, fn2, array) {
+  return fn.apply(fn2, array);
+}
+
+function returnNewFunctionOf(fn, fn2) {
+  return fn.bind(fn2);
+}
